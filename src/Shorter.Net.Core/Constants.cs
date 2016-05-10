40b475fd8142add737102821507bff0022f9af0d
Mark Lopez <m@silvenga.com>
2016-05-09 21:43:34 -0500

    Using hashids and EF6.

---
+++ src/Shorter.Net.Core/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Shorter.Net.Core
+{
+    public class Constants
+    {
+        /// <summary>
+        /// Inspired by z-base-32
+        /// </summary>
+        public const string CharacterSet = "ybndrfg8ejkmcpqxot1uwisza345h769";
+    }
+}
---
+++ src/Shorter.Net.Data/Models/ShortenedUrl.cs
@@ -0,0 +1,16 @@
+﻿namespace Shorter.Net.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class ShortenedUrl
+    {
+        public long ShortenedUrlId { get; set; }
+
+        [Index, StringLength(255)]
+        public string Long { get; set; }
+
+        [Index, StringLength(32)]
+        public string Alias { get; set; }
+    }
+}
